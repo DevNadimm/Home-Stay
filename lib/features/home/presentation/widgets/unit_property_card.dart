0b6249df96aeac57cdfa13f7b5b878b9947056e7
DevNadimm <nadimchowdhury87@gmail.com>
2025-06-23 23:08:56 +0600

    initial commit

--- lib/features/home/presentation/widgets/unit_property_card.dart
+++ lib/features/home/presentation/widgets/unit_property_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:home_stay/core/constants/colors.dart';
 import 'package:home_stay/core/utils/helper_functions/calc_avg_rating.dart';
 import 'package:home_stay/core/utils/helper_functions/calc_discount.dart';
 import 'package:home_stay/features/home/domain/entities/property/property.dart';
+import 'package:home_stay/features/home/presentation/screens/property_view_screen.dart';
 
 class UnitPropertyCard extends StatelessWidget {
   final Property property;
@@ -12,131 +14,134 @@ class UnitPropertyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: AppColors.cardColor,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(width: 1, color: AppColors.inputBorderColor.withOpacity(0.7)),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
-                child: Image.network(
-                  property.images[0],
-                  height: MediaQuery.sizeOf(context).width / 1.8,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                right: 6,
-                top: 6,
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    CupertinoIcons.heart,
-                    color: AppColors.cardColor,
-                    size: 22,
-                  ),
-                ),
-              ),
-              if (property.discountPercentage != null && property.discountPercentage! > 0)
-                Positioned(
-                  top: 12,
-                  left: -26,
-                  child: Transform.rotate(
-                    angle: -0.785398,
-                    child: Container(
-                      width: 100,
-                      padding: const EdgeInsets.symmetric(vertical: 4),
-                      color: Colors.redAccent,
-                      alignment: Alignment.center,
-                      child: Text(
-                        '${property.discountPercentage!.toStringAsFixed(0)}% OFF',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return GestureDetector(
+      onTap: () => Get.to(() => PropertyViewScreen(property: property)),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.cardColor,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(width: 1, color: AppColors.inputBorderColor.withOpacity(0.7)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
               children: [
-                Text(
-                  property.reviews.isEmpty
-                      ? "✨ New"
-                      : "⭐ ${calculateAverageRating(property.reviews).toStringAsFixed(1)}/5 (${property.reviews.length}) Reviews",
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.secondaryFontColor,
+                ClipRRect(
+                  borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                  child: Image.network(
+                    property.images[0],
+                    height: MediaQuery.sizeOf(context).width / 1.8,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                const SizedBox(height: 4),
-                Text(
-                  property.name,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: AppColors.primaryFontColor,
+                Positioned(
+                  right: 6,
+                  top: 6,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      CupertinoIcons.heart,
+                      color: AppColors.cardColor,
+                      size: 22,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 4),
-                Text(
-                  property.location,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    color: AppColors.secondaryFontColor,
-                  ),
-                ),
-                const SizedBox(height: 4),
-                Row(
-                  children: [
-                    if (property.discountPercentage != null && property.discountPercentage! > 0)
-                      Text(
-                        "${property.costPerNight.toStringAsFixed(0)} Tk",
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: AppColors.grey,
-                          decoration: TextDecoration.lineThrough,
+                if (property.discountPercentage != null && property.discountPercentage! > 0)
+                  Positioned(
+                    top: 12,
+                    left: -26,
+                    child: Transform.rotate(
+                      angle: -0.785398,
+                      child: Container(
+                        width: 100,
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        color: Colors.redAccent,
+                        alignment: Alignment.center,
+                        child: Text(
+                          '${property.discountPercentage!.toStringAsFixed(0)}% OFF',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
                         ),
                       ),
-                    Text(
-                      " ${calculateFinalPrice(property.costPerNight, discount: property.discountPercentage).toStringAsFixed(0)} Tk",
-                      style: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.primaryFontColor,
-                      ),
                     ),
-                    const Text(
-                      ' / Night',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                        color: AppColors.secondaryFontColor,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
               ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    property.reviews.isEmpty
+                        ? "✨ New"
+                        : "⭐ ${calculateAverageRating(property.reviews).toStringAsFixed(1)}/5 (${property.reviews.length}) Reviews",
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.secondaryFontColor,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    property.name,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.primaryFontColor,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    property.location,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: AppColors.secondaryFontColor,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Row(
+                    children: [
+                      if (property.discountPercentage != null && property.discountPercentage! > 0)
+                        Text(
+                          "${property.costPerNight.toStringAsFixed(0)} Tk",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.grey,
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                        ),
+                      Text(
+                        " ${calculateFinalPrice(property.costPerNight, discount: property.discountPercentage).toStringAsFixed(0)} Tk",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primaryFontColor,
+                        ),
+                      ),
+                      const Text(
+                        ' / Night',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: AppColors.secondaryFontColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
